f3e9d84eb492f227472831031845d7eefead583a
wisasoft <wisasoft001@gmail.com>
2021-07-01 15:16:59 +0700

    1072021

--- server.js
+++ server.js
@@ -45,10 +45,10 @@ io.on('connection', socket => {
   });
 
   // Listen for chatMessage
-  socket.on('chatMessage', msg => {
+  socket.on('chatMessage', {msg} => {
     const user = getCurrentUser(socket.id);
 
-    io.to(user.room).emit('message', formatMessage(user.username, msg));
+    io.to(user.room).emit('message', formatMessage(user.fullname, msg.msg));
   });
 
   // Runs when client disconnects
